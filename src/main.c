6c4a5ea0e286f79f593708b8af7c9a4f688adc47
AngeTenShi <megazombie06pro@gmail.com>
2022-07-25 23:30:43 +0200

    adding parsing

--- src/main.c
+++ src/main.c
@@ -17,20 +17,12 @@ int	main(int ac, char **av)
 	t_all	*var;
 
 	var = malloc(sizeof(t_all));
-	if (ac < 5 || ac > 6)
-	{
-		ft_putstr_fd("Error it must be 5 arguments\n", 2);
-		free(var);
+	if (!parse_args(ac, av, var))
 		return (0);
-	}
-	if (ac == 5)
-		init_rules(var, av, 0);
-	else
-		init_rules(var, av, 1);
 	init_mutex(var);
 	init_philos(var);
 	create_threads(var);
 	free(var);
 	//return (free_everything(&var));
-	return (0);
+	return (1);
 }
